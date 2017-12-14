f4e06a852aac4db19846b88e0c2f717777336cf9
Christian G. Warden <cwarden@xerus.org>
2017-12-14 11:49:49 -0800

    Allow Batch Size To Be Modified. Return Failures.
    Use a new BulkJob struct that embeds force.JobInfo.  Expose a BatchSize
    field that an option function to batch.Run can use to change the batch
    size.
    
    Return the number of failures after batch.Run completes.

--- update.go
+++ update.go
@@ -8,31 +8,29 @@ import (
 	"time"
 )
 
-type Operation int
+type BulkJob struct {
+	force.JobInfo
+	BatchSize int
+}
 
-const (
-	Update Operation = iota
-	Upsert
-)
-
-func update(session *force.Force, records <-chan force.ForceRecord, done chan<- bool, jobOptions ...func(*force.JobInfo)) {
-	job := force.JobInfo{
-		Operation:   "update",
-		ContentType: "JSON",
+func update(session *force.Force, records <-chan force.ForceRecord, failures chan<- int, jobOptions ...func(*BulkJob)) {
+	job := BulkJob{
+		BatchSize: 2000,
 	}
+	job.Operation = "update"
+	job.ContentType = "JSON"
 
 	for _, option := range jobOptions {
 		option(&job)
 	}
 
-	jobInfo, err := session.CreateBulkJob(job)
+	jobInfo, err := session.CreateBulkJob(job.JobInfo)
 	if err != nil {
 		fmt.Println("Failed to create bulk job: " + err.Error())
 		os.Exit(1)
 	}
 	fmt.Printf("Created job %s\n", jobInfo.Id)
-	batchSize := 2000
-	batch := make([]force.ForceRecord, 0, batchSize)
+	batch := make([]force.ForceRecord, 0, job.BatchSize)
 
 	sendBatch := func() {
 		updates, err := json.Marshal(batch)
@@ -45,12 +43,12 @@ func update(session *force.Force, records <-chan force.ForceRecord, done chan<- 
 		if err != nil {
 			fmt.Println("Failed to enqueue batch: " + err.Error())
 		}
-		batch = make([]force.ForceRecord, 0, 10000)
+		batch = make([]force.ForceRecord, 0, job.BatchSize)
 	}
 
 	for record := range records {
 		batch = append(batch, record)
-		if len(batch) == batchSize {
+		if len(batch) == job.BatchSize {
 			sendBatch()
 		}
 	}
@@ -62,8 +60,9 @@ func update(session *force.Force, records <-chan force.ForceRecord, done chan<- 
 		fmt.Println("Failed to close bulk job: " + err.Error())
 		os.Exit(1)
 	}
+	var status force.JobInfo
 	for {
-		status, err := session.GetJobInfo(jobInfo.Id)
+		status, err = session.GetJobInfo(jobInfo.Id)
 		if err != nil {
 			fmt.Println("Failed to get bulk job status: " + err.Error())
 			os.Exit(1)
@@ -74,7 +73,7 @@ func update(session *force.Force, records <-chan force.ForceRecord, done chan<- 
 		}
 		time.Sleep(2000 * time.Millisecond)
 	}
-	done <- true
+	failures <- status.NumberRecordsFailed
 }
 
 func processRecords(input <-chan force.ForceRecord, output chan<- force.ForceRecord, converter func(force.ForceRecord) []force.ForceRecord) {
@@ -87,26 +86,26 @@ func processRecords(input <-chan force.ForceRecord, output chan<- force.ForceRec
 	close(output)
 }
 
-func Run(sobject string, query string, converter func(force.ForceRecord) []force.ForceRecord, jobOptions ...func(*force.JobInfo)) {
+func Run(sobject string, query string, converter func(force.ForceRecord) []force.ForceRecord, jobOptions ...func(*BulkJob)) int {
 	session, err := force.ActiveForce()
 	if err != nil {
 		os.Exit(1)
 	}
 
-	setObject := func(job *force.JobInfo) {
+	setObject := func(job *BulkJob) {
 		job.Object = sobject
 	}
-	jobOptions = append([]func(*force.JobInfo){setObject}, jobOptions...)
+	jobOptions = append([]func(*BulkJob){setObject}, jobOptions...)
 
 	queried := make(chan force.ForceRecord)
 	updates := make(chan force.ForceRecord)
-	doneUpdating := make(chan bool)
+	numberFailers := make(chan int)
 	go processRecords(queried, updates, converter)
-	go update(session, updates, doneUpdating, jobOptions...)
+	go update(session, updates, numberFailers, jobOptions...)
 	err = session.QueryAndSend(query, queried)
 	if err != nil {
 		fmt.Println("Query failed: " + err.Error())
 		os.Exit(1)
 	}
-	<-doneUpdating
+	return <-numberFailers
 }
